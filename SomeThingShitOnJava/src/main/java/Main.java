96e0dae085a4c4280a1142aca2346d483807bdc3
RoflPolarity <ignis604@gmail.com>
2022-09-23 16:44:14 +0300

    test gateway

--- SomeThingShitOnJava/src/main/java/Main.java
+++ SomeThingShitOnJava/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import py4j.GatewayServer;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String ... str){
-        GatewayServer server = new GatewayServer();
+        GatewayServer server = new GatewayServer(new ArrayList<String>());
         server.start();
     }
 }
