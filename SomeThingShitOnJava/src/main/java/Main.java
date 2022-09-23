9973dffcbfc881329f708f5afff9c302a1065eb3
RoflPolarity <ignis604@gmail.com>
2022-09-23 16:36:38 +0300

    Get Gateway Server for Python ready

--- SomeThingShitOnJava/src/main/java/Main.java
+++ SomeThingShitOnJava/src/main/java/Main.java
@@ -1,2 +1,8 @@
+import py4j.GatewayServer;
+
 public class Main {
+    public static void main(String ... str){
+        GatewayServer server = new GatewayServer();
+        server.start();
+    }
 }
